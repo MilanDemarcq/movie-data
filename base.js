a5696fd1dbea0213ab0d2f0e2335971472c5921b
MilanDemarcq <milan.demarcq@gmail.com>
2018-03-14 10:25:57 +0100

    Correct 4-spaces indent

--- base.js
+++ base.js
@@ -1,12 +1,12 @@
 
 //// GLOBAL VARIABLES
 
- // Airtable App ID or Base: appqgOIJurd9Tr0L4
-  var baseID = "appqgOIJurd9Tr0L4";
-  // Airtable API URL
-  var apiURL = "https://api.airtable.com/v0/";
-  // Airtable Table URL
-  var tableURL = "/Table%201";
+// Airtable App ID or Base: appqgOIJurd9Tr0L4
+var baseID = "appqgOIJurd9Tr0L4";
+// Airtable API URL
+var apiURL = "https://api.airtable.com/v0/";
+// Airtable Table URL
+var tableURL = "/Table%201";
 
 
 //// MAIN
@@ -30,24 +30,23 @@ $(function() {
         });
     });
 
-    // TODO INDENT THAT SHIT
+    // Once API Key is known, API calls can be made.
+    $('h1').queue("operations", function(){
 
-  // Once API Key is known, API calls can be made.
-  $('h1').queue("operations", function(){
-
-    // Get full base from Airtable, display it and get the size
-    getfullAirtableData(apikey, function(full_base_length){
-      // Display total count in the corresponding info div
-      $('#TotalCount').append(full_base_length);
-    });
+        // Get full base from Airtable, display it and get the size
+        getfullAirtableData(apikey, function(full_base_length){
+            // Display total count in the corresponding info div
+            $('#TotalCount').append(full_base_length);
+        });
 
     // VISION Stats
     getVisionData(apikey)
 
-  });
+    });
 
-  // Init the sequence of queued events
-  $('h1').dequeue("operations");
+
+    // Init the sequence of queued events
+    $('h1').dequeue("operations");
 
   
 });
@@ -58,52 +57,55 @@ $(function() {
 // Gets data about vision techniques
 function getVisionData(apikey){
 
-  apiGetwFormula(apikey, "AND(Vision = \"NX\")", function(response_data){
+    apiGetwFormula(apikey, "AND(Vision = \"NX\")", function(response_data){
 
-     parseBase(response_data, "#testarray2");
+        parseBase(response_data, "#testarray2");
 
-  });
+    });
 
 }
 
-//
-function apiGetwFormula(apikey, formula, callback){
 
-  // Get function, using Axios.js
-  axios.get(
+/////////////////////////////////////////////////////////////
+// Generic API Get function with filterbyformula parameter
+function apiGetwFormula(apikey, formula, callback){
+/////////////////////////////////////////////////////////////
+
+    // Get function, using Axios.js
+    axios.get(
 
     // Access the Table in the Base, and use the view "Grid View"
     //apiURL + baseID + tableURL + "?view=Grid%20view",
     apiURL + baseID + tableURL,
     { 
-      headers: {Authorization: "Bearer " + apikey},
-      params: {
+        headers: {Authorization: "Bearer " + apikey},
+        params: {
         // Filter using the formula in parameter
         filterByFormula: formula,
-      }
-
+        }
     }
-  ).then(function(response) {
-    // Send data back to callback
-    callback(response);
+    ).then(function(response) {
+        // Send data back to callback
+        callback(response);
 
-  }).catch(function(error) {
-    // Handle error cases
-    $('#texteJQ').append(error);
-  });  
+    }).catch(function(error) {
+        // Handle error cases
+        $('#texteJQ').append(error);
+    });  
 
 }
+
 
 ///////////////////////////////////////////////////////////////////
 // Reads the API Key in the specified file and sends it back
 function getApiKeyFromFile(filename, callback){
 ///////////////////////////////////////////////////////////////////
 
-  var loadingkey = $('<div>');
-  // Loading the content of the given file
-  loadingkey.load(filename, function(response, status){
-    callback(response);
-  });
+    var loadingkey = $('<div>');
+    // Loading the content of the given file
+    loadingkey.load(filename, function(response, status){
+        callback(response);
+    });
 }
 
 
@@ -112,35 +114,34 @@ function getApiKeyFromFile(filename, callback){
 function getfullAirtableData(apikey, callback){
 ///////////////////////////////////////////////////////////////////
 
-  // Main get function, using Axios.js
-  axios.get(
+    // Main get function, using Axios.js
+    axios.get(
 
-    // Access the Table in the Base, and use the view "Grid View"
-    //apiURL + baseID + tableURL + "?view=Grid%20view",
-    apiURL + baseID + tableURL,
-    { 
-        headers: {Authorization: "Bearer " + apikey},
-        params: {
-          //maxRecords: 10,
-          view: "Grid view",
-          //filterByFormula: 'AND(Year > 2000, Vision = "NX")',
+        // Access the Table in the Base, and use the view "Grid View"
+        //apiURL + baseID + tableURL + "?view=Grid%20view",
+        apiURL + baseID + tableURL,
+        { 
+            headers: {Authorization: "Bearer " + apikey},
+            params: {
+              //maxRecords: 10,
+              view: "Grid view",
+              //filterByFormula: 'AND(Year > 2000, Vision = "NX")',
+            }
+
         }
+    ).then(function(response) {
+        // Handle the response data
 
-    }).then(function(response) {
-      // Handle the response data
-
-      parseBase(response, "#testarray", function(full_base_length){
-
-        callback(full_base_length);
-        //alert(full_base_length);
-
-      });
+        parseBase(response, "#testarray", function(full_base_length){
+            callback(full_base_length);
+        });
 
     }).catch(function(error) {
-      // Handle error cases
-       $('#texteJQ').append("error<br>");
-       $('#texteJQ').append(error);
+        // Handle error cases
+        $('#texteJQ').append("error<br>");
+        $('#texteJQ').append(error);
     });  
+
 }
 
 
@@ -150,25 +151,25 @@ function getfullAirtableData(apikey, callback){
 function parseBase(response_data, parselocation, callback) {
 /////////////////////////////////////////////////////////////////////////////////////
 
-  // Get all the records in an Array like fashion.
-  var full_unmaped_array = Object.values(response_data.data.records);
-  // This cannot be used directly since the intresting data is in response_data.data.records[x].fields
-  // But is useful to get the number of records.
+    // Get all the records in an Array like fashion.
+    var full_unmaped_array = Object.values(response_data.data.records);
+    // This cannot be used directly since the intresting data is in response_data.data.records[x].fields
+    // But is useful to get the number of records.
 
-  //$('#testarray').append(full_unmaped_array.length); $('#testarray').append("<br>");
+    //$('#testarray').append(full_unmaped_array.length); $('#testarray').append("<br>");
 
-  // Declare new array of correct length (first dimension)
-  var full_maped_array = new Array(full_unmaped_array.length);
+    // Declare new array of correct length (first dimension)
+    var full_maped_array = new Array(full_unmaped_array.length);
 
-  // Loop on all records to construct the 2nd dimension of the array with the content of .fields.
-  for(i=0; i<full_unmaped_array.length; i++){
-    full_maped_array[i]= Object.values(response_data.data.records[i].fields);
-  }
+    // Loop on all records to construct the 2nd dimension of the array with the content of .fields.
+    for(i=0; i<full_unmaped_array.length; i++){
+        full_maped_array[i]= Object.values(response_data.data.records[i].fields);
+     }
 
-  // Print everything
-  print2DArray(full_maped_array, parselocation);
+    // Print everything
+    print2DArray(full_maped_array, parselocation);
 
-   if(callback) callback(full_unmaped_array.length);
+    if(callback) callback(full_unmaped_array.length);
 
 }
 
@@ -178,17 +179,17 @@ function parseBase(response_data, parselocation, callback) {
 function print2DArray(myarray, display_element_id){
 //////////////////////////////////////////////////////
 
-  $(display_element_id).append("Table Content : <br>");
+    $(display_element_id).append("Table Content : <br>");
 
-  $.each(myarray, function(index,value){    
+    $.each(myarray, function(index,value){    
 
-    $.each(value, function(index2, value2){
-      $(display_element_id).append(value2);
-      if (index2 != (value.length - 1)){$(display_element_id).append(", ");}
-    })
+        $.each(value, function(index2, value2){
+            $(display_element_id).append(value2);
+            if (index2 != (value.length - 1)){$(display_element_id).append(", ");}
+        })
 
-    $(display_element_id).append("<br>");    
-  });
+        $(display_element_id).append("<br>");    
+    });
 
 }
 
@@ -198,11 +199,11 @@ function print2DArray(myarray, display_element_id){
 function print1DArray(myarray, display_element_id){
 //////////////////////////////////////////////////////
 
-  $(display_element_id).append("Table Content : <br>");
+    $(display_element_id).append("Table Content : <br>");
  
-  $.each(myarray, function(index, value){
-    $(display_element_id).append(value);
-    if (index != (myarray.length - 1)){$(display_element_id).append(", ");}
-  })
+    $.each(myarray, function(index, value){
+        $(display_element_id).append(value);
+        if (index != (myarray.length - 1)){$(display_element_id).append(", ");}
+    })
 
 }
